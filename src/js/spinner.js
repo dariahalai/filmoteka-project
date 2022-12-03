cc4e4a59c3fe975babbc5940547728d42c952716
Vovandir <106840463+Vovandir@users.noreply.github.com>
2022-12-03 17:58:53 +0200

    little change

--- src/js/spinner.js
+++ src/js/spinner.js
@@ -2,5 +2,5 @@ export default function spinnerToggle() {
   document.querySelector('.spinner').classList.toggle("is-hidden");
 }
 
-// потрібно викликати функцію spinnerToggle(), для цього у js файлі до якого функція викликається потрібно зробити імпорт:     import spinnerToggle from './js/spinner';
+// потрібно викликати функцію spinnerToggle(), для цього у js файлі до якого функція викликається потрібно зробити імпорт:     import spinnerToggle from './js/spinner'(адреса може бути інша);
 // Функція просто забирає у спінера display:none, тому коли сторінка завантажиться потрібно знову викликати функцію.
