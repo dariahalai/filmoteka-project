21b6c9ff31671df3c10e52873c9c6e518fa53932
Barchuk Andrii <abarchuk1976@gmail.com>
2022-12-03 08:54:12 +0200

    commit

--- src/popular.js
+++ src/popular.js
@@ -3,26 +3,31 @@ import axios from 'axios';
 const KEY = '9068359f92c010fa6a3cf763f10a0606';
 const MEDIA_TYPE = 'movie';
 const TIME_WINDOW = 'week';
-const API = 'https://api.themoviedb.org/3/trending';
+const API = 'https://api.themoviedb.org/3/';
+const TRENDING = 'trending';
+const GENRES = 'genre/movie/list';
+const KEY_GENRES = 'current-genres';
 const IMG_PATH = 'https://image.tmdb.org/t/p/';
-const FULL_SIZE = 'original';
+const LARGE_SIZE = 'original';
 const SMALL_SIZE = 'w500';
 
 let currentPage = 0;
 let totalPages = 0;
+let genresLoaded = false;
 
-const gallaryRef = document.querySelector('.js-gallary');
+const galleryRef = document.querySelector('.js-gallery');
 
-window.addEventListener('load', evt => {
-  gallaryRef.setHTML('');
+window.addEventListener('load', () => {
+  galleryRef.setHTML('');
 
   getPopulars(1).then(response => {
     const { page, results, total_pages: pages } = response;
-    if (!pages) {
-      currentPage = page;
-      totalPages = pages;
-    }
-    // renderFilmCards(results);
+    currentPage = page;
+    totalPages = pages;
+
+    console.log(response);
+
+    renderFilmCards(results);
     // renderPagination(page, pages);
   });
 });
@@ -35,7 +40,7 @@ async function getPopulars(page) {
     });
 
     const response = await axios.get(
-      `${API}/${MEDIA_TYPE}/${TIME_WINDOW}?${searchParams}`
+      `${API}${TRENDING}/${MEDIA_TYPE}/${TIME_WINDOW}?${searchParams}`
     );
 
     if (response.status !== 200) {
@@ -45,5 +50,54 @@ async function getPopulars(page) {
     return response.data;
   } catch (error) {
     console.log('Підставити картинку, сервер терміново недоступний');
+  }
+}
+
+function renderFilmCards(data) {
+  let markup = data.map(
+    ({
+      backdrop_path,
+      poster_path,
+      genre_ids,
+      id,
+      title,
+      overview,
+      release_date,
+    }) => {
+      let genres = getGenres(genre_ids);
+      if (!title) title = 'no information';
+      let year = release_date.substring(0, 4);
+      let largeImg = IMG_PATH + LARGE_SIZE + backdrop_path;
+      let smallImg = IMG_PATH + SMALL_SIZE + backdrop_path;
+      console.log(genre_ids, title, year, largeImg, smallImg);
+    }
+  );
+}
+
+async function getOriginGenres() {
+  try {
+    const searchParams = new URLSearchParams({
+      api_key: KEY,
+    });
+
+    const response = await axios.get(`${API}${GENRES}?${searchParams}`);
+
+    if (response.status !== 200) {
+      throw new Error(response.status);
+    }
+
+    return response.data;
+  } catch (error) {
+    console.log('Підставити картинку, сервер терміново недоступний');
+  }
+}
+
+function getGenres(arr) {
+  if (!genresLoaded) {
+    getOriginGenres().then(response => {
+      localStorage.setItem(KEY_GENRES, JSON.stringify(response));
+      genresLoaded = true;
+      // return response.map(({ name, id }) => arr.includes(id)).join(', ');
+    });
   }
 }
