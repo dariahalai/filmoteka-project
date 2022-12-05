2bcd25704714fdd96e20820bc8ef2b3ceb22f384
Oleh.Scherbak <oleh.scherbak@ukr.net>
2022-12-05 19:17:54 +0200

    add initial files to team modal

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import './js/pagination';
 import './js/popular';
 import './js/film-search';
 import './js/open-model';
+import './js/modal-team';
---
+++ src/js/modal-team.js
@@ -0,0 +1,31 @@
+// $ npm install @splidejs/splide
+
+
+// import Splide from '@splidejs/splide';
+// import '@splidejs/splide/dist/css/themes/splide-skyblue.min.css';
+// teamSplide = new Splide('.splide');
+// teamSplide.mount();
+// console.log(teamSplide);
+// console.log(teamSplide.defaults);
+// teamSplide.defaults = {
+//   type: 'fade',
+//   perPage: 4,
+//     rewind: true,
+//     width: 85%,
+// };
+
+// const teamModalLink = document.querySelector('.team-modal-link');
+// const modal = document.querySelector('.data-modal-team');
+// const modalCloseBtn = document.querySelector('.modal-team__close-btn');
+
+// teamModalLink.addEventListener('click', teamModalShow);
+// modalCloseBtn.addEventListener('click', onCloseBtnClick);
+// function teamModalShow(evt) {
+//   evt.preventDefault();
+//   modal.classList.toggle('is-hidden');
+//   document.body.classList.toggle('body--modal-open');
+// }
+// function onCloseBtnClick() {
+//   modal.classList.toggle('is-hidden');
+//   document.body.classList.toggle('body--modal-open');
+// }
