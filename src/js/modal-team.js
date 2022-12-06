bc4e5ee9a94ca2bb1318c7610ee933e76e8d2007
Oleh.Scherbak <oleh.scherbak@ukr.net>
2022-12-06 02:07:39 +0200

    add slider

--- src/js/modal-team.js
+++ src/js/modal-team.js
@@ -1,31 +1,45 @@
 // $ npm install @splidejs/splide
 
+import Splide from '@splidejs/splide';
+import '@splidejs/splide/dist/css/themes/splide-skyblue.min.css';
 
-// import Splide from '@splidejs/splide';
-// import '@splidejs/splide/dist/css/themes/splide-skyblue.min.css';
-// teamSplide = new Splide('.splide');
+const teamSplide = new Splide('.splide');
+teamSplide.mount();
+console.log(Splide.defaults);
+console.log(teamSplide.defaults);
+
 // teamSplide.mount();
 // console.log(teamSplide);
 // console.log(teamSplide.defaults);
-// teamSplide.defaults = {
-//   type: 'fade',
-//   perPage: 4,
-//     rewind: true,
-//     width: 85%,
-// };
+Splide.defaults = {
+  type: 'slide',
+  perPage: 4,
+  rewind: true,
+  keyboard: 'global',
+  width: 500,
+};
+console.log(Splide.defaults);
 
-// const teamModalLink = document.querySelector('.team-modal-link');
-// const modal = document.querySelector('.data-modal-team');
-// const modalCloseBtn = document.querySelector('.modal-team__close-btn');
+const teamModalLink = document.querySelector('.team-modal-link');
+const modal = document.querySelector('.data-modal-team');
+const modalCloseBtn = document.querySelector('.modal-team__close-btn');
 
-// teamModalLink.addEventListener('click', teamModalShow);
-// modalCloseBtn.addEventListener('click', onCloseBtnClick);
-// function teamModalShow(evt) {
-//   evt.preventDefault();
-//   modal.classList.toggle('is-hidden');
-//   document.body.classList.toggle('body--modal-open');
-// }
-// function onCloseBtnClick() {
-//   modal.classList.toggle('is-hidden');
-//   document.body.classList.toggle('body--modal-open');
-// }
+teamModalLink.addEventListener('click', teamModalShow);
+modalCloseBtn.addEventListener('click', onCloseBtnClick);
+
+function teamModalShow(evt) {
+  evt.preventDefault();
+  modal.classList.toggle('is-hidden');
+  document.body.classList.toggle('body--modal-open');
+  document.addEventListener('keydown', onEscModalTeam);
+}
+function onEscModalTeam(evt) {
+  if (evt.code === 'Escape') {
+    onCloseBtnClick();
+    document.removeEventListener('keydown', onEscModalTeam);
+  }
+}
+function onCloseBtnClick() {
+  modal.classList.toggle('is-hidden');
+  document.body.classList.toggle('body--modal-open');
+}
