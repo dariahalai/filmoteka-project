0a91a662ff2734d5bab9bc50f62c8591d8b43d87
OlehScherbak <103534824+OlehScherbak@users.noreply.github.com>
2022-12-09 15:05:45 +0200

    grid and animation

--- src/js/modal-team.js
+++ src/js/modal-team.js
@@ -1,16 +1,29 @@
-// $ npm install @splidejs/splide
+// npm install @splidejs/splide
+// npm install @splidejs/splide-extension-grid
 
 import Splide from '@splidejs/splide';
+import { Grid } from '@splidejs/splide-extension-grid';
 import '@splidejs/splide/dist/css/themes/splide-sea-green.min.css';
 
 const teamSplide = new Splide('.splide', {
   type: 'slide',
-  perPage: 4,
-  rewind: true,
+  // perPage: 4,
+  rewind: false,
+  height: '290px',
+  width: '900px',
+  gap: '5px',
   keyboard: 'global',
-  gap: 15,
   arrows: true,
   pagination: false,
+  grid: {
+    dimensions: [
+      [1, 4],
+      [1, 4],
+      [1, 4],
+      [1, 1],
+    ],
+    gap: { row: '1em', col: '1em' },
+  },
   mediaQuery: 'max',
   breakpoints: {
     767: {
@@ -18,14 +31,20 @@ const teamSplide = new Splide('.splide', {
       perPage: 2,
       perMove: 1,
       height: 650,
+      grid: false,
+    },
+    1279: {
+      height: '210px',
+      width: '700px',
     },
   },
 });
-teamSplide.mount();
+teamSplide.mount({ Grid });
 
 const teamModalLink = document.querySelector('.team-modal-link');
 const modal = document.querySelector('.data-modal-team');
 const modalPoster = document.querySelector('.poster-modal');
+const modalPosterImg = document.querySelector('.modal-poster-img ');
 const modalCloseBtn = document.querySelector('.modal-team__close-btn');
 
 teamModalLink.addEventListener('click', teamModalShow);
@@ -35,12 +54,14 @@ modal.addEventListener('click', onBackdropClick);
 function teamModalShow(evt) {
   evt.preventDefault();
   modalPoster.classList.remove('is-hidden');
+  modalPosterImg.classList.remove('is-hidden');
   setTimeout(() => {
     modalPoster.classList.add('is-hidden');
+    modalPosterImg.classList.add('is-hidden');
+    modal.classList.toggle('is-hidden');
+    // document.body.classList.toggle('body--modal-open');
   }, 2000);
 
-  modal.classList.toggle('is-hidden');
-  document.body.classList.toggle('body--modal-open');
   document.addEventListener('keydown', onEscModalTeam);
 }
 function onEscModalTeam(evt) {
